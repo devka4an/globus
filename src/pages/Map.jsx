b6ec1471184587aa26cc6120d03ec8ba9ab915df
Alisher <ameldebek@jmart.kz>
2021-06-22 19:44:28 +0600

    done done

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import SvgWrapper from 'react-interactive-svg'
-import { ReactComponent as Svg } from './../map.svg'
-
 export default function Map() {
     return (
         <div className="map">
             MAP
 
-            <div className="map-wrapper">
-                <SvgWrapper
-                    hoverBorderColor='#FFFF'
-                    activeColor='green'
-                    defaultHoverBorder='#000'
-                    allowedShapes={["polygon"]}
-                >
-                    <Svg ref={React.createRef()} />
-                </SvgWrapper>
-            </div>
+
         </div>
     )
 }
